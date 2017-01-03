19f9c8c4db1437a4bf96c33e59650f81832b134f
kravelin <kele.kravelin@gmail.com>
2017-01-02 21:25:59 -0800

    finished

--- main.py
+++ main.py
@@ -15,11 +15,87 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+import re
 
-class MainHandler(webapp2.RequestHandler):
+from string import letters
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+class BlogHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        return render_str(template, **params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+def render_post(response, post):
+    response.out.write('<b>' + post.title + '</b><br>')
+    response.out.write('<pre>' + post.content + '</pre>')
+
+class MainPage(BlogHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.write('Hello, Udacity!')
+
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+
+class Post(db.Model):
+    title = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
+
+class BlogFront(BlogHandler):
+    def get(self):
+        posts = db.GqlQuery("SELECt * FROM Post ORDER BY created DESC LIMIT 10")
+        self.render('frontpage.html', posts=posts)
+
+class PostPage(BlogHandler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("permalink.html", post = post)
+
+class NewPost(BlogHandler):
+    def get(self):
+        self.render("newpost.html")
+
+    def post(self):
+        title = self.request.get('title')
+        content = self.request.get('content')
+
+        if title and content:
+            p = Post(parent = blog_key(), title = title, content = content)
+            p.put()
+            self.redirect('/%s' % str(p.key().id()))
+        else:
+            error = "title and content please!"
+            self.render("newpost.html", title=title, content=content,
+           error=error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', BlogFront),
+    ('/newpost.html', NewPost),
+    ('/([0-9]+)', PostPage)
 ], debug=True)
