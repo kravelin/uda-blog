f8c957a13cfc233ff16a1baac00e50c54d4fb4aa
kravelin <kele.kravelin@gmail.com>
2017-01-28 09:44:24 -0800

    added comment support, refactored

---
+++ blogData.py
@@ -0,0 +1,93 @@
+﻿import validate
+import jinja2
+import os
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+
+### Database setup
+def users_key(group = "default"):
+    return db.Key.from_path("users", group)
+
+
+class User(db.Model):
+    name = db.StringProperty(required = True)
+    pw_hash = db.StringProperty(required = True)
+    email = db.StringProperty()
+
+
+    @classmethod
+    def by_id(cls, uid):
+        return cls.get_by_id(uid, parent = users_key())
+
+
+    @classmethod
+    def by_name(cls, name):
+        u = cls.all().filter("name =", name).get()
+        return u
+
+
+    @classmethod
+    def register(cls, name, pw, email = None):
+        pw_hash = validate.make_pw_hash(name, pw)
+        return cls(parent = users_key(),
+                    name = name,
+                    pw_hash = pw_hash,
+                    email = email)
+
+
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and validate.valid_pw(name, pw, u.pw_hash):
+            return u
+
+
+class Comments(db.Model):
+    post_id = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    author = db.StringProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+
+    def render(self, username):
+        self._render_text = self.content
+        return self.render_str("comment.html", c = self, username = username)
+
+
+    @classmethod
+    def by_post(cls, post_id):
+        c = cls.all().filter("post_id =", str(post_id))
+        c_count = cls.all(keys_only=True).filter("post_id =", str(post_id)).count(5000)
+        return c, c_count
+
+
+class Post(db.Model):
+    title = db.StringProperty(required = True)
+    author = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+
+    def render(self, username):
+        self._render_text = self.content
+        return self.render_str("post.html", p = self, username = username)
+
+
+def blog_key(name = "default"):
+    return db.Key.from_path("blogs" , name)
---
+++ validate.py
@@ -0,0 +1,75 @@
+﻿import string
+import hashlib
+import hmac
+import re
+import random
+
+from string import letters
+
+SECRET = "ajf9a0jf32kj532mrafsfjfI(#$#$)5kjdsjfa0wer59"
+
+
+### salted passwords
+def make_salt(length = 5):
+    return ''.join(random.choice(letters) for x in xrange(length))
+
+
+def make_pw_hash(name, pw, salt = None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name+pw+salt).hexdigest()
+    return "%s|%s" % (salt, h)
+
+
+### validate username, password, email during signup proccess
+def valid_username(username):
+    user_re = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+    return user_re.match(username)
+
+
+def valid_password(password):
+    pwd_re = re.compile(r"^.{3,20}$")
+    return pwd_re.match(password)
+
+
+def valid_email(email):
+    email_re = re.compile("^[\S]+@[\S]+.[\S]+$")
+    if email_re.match(email) or email == "":
+        return True
+    else:
+        return False
+
+
+def valid_pw(name, password, h):
+    salt = h.split('|')[0]
+    return h == make_pw_hash(name, password, salt)
+
+
+def check_cookie(uncookie):
+    username, hashedpw = uncookie.split('|')
+    check = db.GqlQuery("SELECT * FROM Users WHERE name='%s'" % username)
+
+    username = str(username)
+    hashedpw = str(hashedpw)
+
+    if check:
+        for row in check:
+            hashpw = row.password_hash.split('|')[1]
+
+            if str(row.name) == username and str(hashpw) == hashedpw:
+                return True
+
+
+### cookie hashing manipulators
+def hash_str(s):
+    return hmac.new(SECRET,s).hexdigest()
+
+
+def check_secure_val(secure_val):
+    val = secure_val.split('|')[0]
+    if secure_val == make_secure_val(val):
+        return val
+
+
+def make_secure_val(val):
+    return "%s|%s" % (val, hmac.new(SECRET, val).hexdigest())
